8edbd1ce48175c35a2c88e808565b2c557a92691
Theodore Richardson <richardson.t@login7.ufhpc>
2024-07-25 08:06:23 -0400

    tweak options

--- dask_runner.py
+++ dask_runner.py
@@ -1,5 +1,5 @@
 import numpy as np
-from os import system
+from astropy.table import Table
 
 import dask
 from dask.distributed import Client
@@ -12,27 +12,38 @@ def run_row(row):
     tot_matrix = make_matrix(row)
     return tot_matrix, row
 
+b_table = Table.read('boundaries.fits')
+nrows = len(b_table)
+del b_table
+
+print('Starting cluster.')
 cluster = SLURMCluster(cores=1,
                        processes=1,
                        memory='72GB',
                        walltime='96:00:00',
                        account='astronomy-dept',
                        job_extra_directives=['--qos=astronomy-dept-b',
-                                             '--output=log/dask/%A.out',
-                                             '--error=log/dask/%A.err'],
+                                             '--output=log/%A.out',
+                                             '--error=log/%A.err'],
                        nanny=True,
-                       worker_extra_args=['--preload /blue/adamginsburg/richardson.t/research/flux/confusion_dir/matrix_functions.py'],
+                       worker_extra_args=['--preload /blue/adamginsburg/richardson.t/research/flux/confusion_dir/matrix_functions.py',
+                                          '--lifetime 119m'],
+                       scheduler_options={'dashboard_address':':13579'},
 )
 
 cluster.adapt(minimum=0,maximum=200)
 client = Client(cluster)
-mapping = client.map(run_row, range(2))
+
+print('Starting mapping.')
+mapping = client.map(run_row, range(nrows))
 delayed = client.gather(mapping)
 
-for res in enumerate(delayed):
-    mat, row = dask.compute(res)
+for res in delayed:
+    output = dask.compute(res)
+    mat = output[0][0]
+    row = output[0][1]
     for d,sub_mat in enumerate(mat):
-        np.save(f'output/dask/{row}_{d}.npy',sub_mat)
+        np.save(f'output/{row}_{d}.npy',sub_mat)
 
 client.close()
 cluster.close()
